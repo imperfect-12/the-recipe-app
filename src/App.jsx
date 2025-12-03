61f885a44f241ff88864573cac1bde5034e4a193
imperfect-12 <b.prashant@iitg.ac.in>
2025-12-03 09:57:05 +0530

    one more minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
 
   const handleSearch = async (e) => {
     e.preventDefault()
-    if (!searchQuery.trim()) return
+    if (!searchQuery.trim()) return getRandomRecipies()
     if (loading) return
 
     setLoading(true)
